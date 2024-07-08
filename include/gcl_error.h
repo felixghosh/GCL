511e4bfeb7b5c2b087e81a0791931450ac42bc3d
Felix Ghosh <felix.ghosh@gmail.com>
2024-07-08 14:56:19 +0200

    Uppdated gcl_error with variadic args

--- include/gcl_error.h
+++ include/gcl_error.h
@@ -3,9 +3,9 @@
 
 #include <stdio.h>
 
-#define error_exit(message) \
+#define error_exit(...) \
             fprintf(stderr, "ERROR AT LINE:%d in FILE: %s: ", __LINE__, __FILE__);\
-            fprintf(stderr, (message));\
+            fprintf(stderr, __VA_ARGS__);\
             exit(1)
 
 #endif
