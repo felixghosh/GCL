93fa258937c7ed58ce1143715ec0c9ff045993b2
Felix Ghosh <felix.ghosh@gmail.com>
2024-07-09 07:56:39 +0200

    Added gcl warning

--- include/gcl_error.h
+++ include/gcl_error.h
@@ -2,10 +2,15 @@
 #define ERROR_H
 
 #include <stdio.h>
+#include "gcl_colors.h"
 
 #define error_exit(...) \
-            fprintf(stderr, "ERROR AT LINE:%d in FILE: %s: ", __LINE__, __FILE__);\
+            fprintf(stderr, "%sERROR AT LINE: %d in FILE: %s: %s", RED, __LINE__, __FILE__, END);\
             fprintf(stderr, __VA_ARGS__);\
             exit(1)
+
+#define warning(...) \
+            fprintf(stderr, "%sWARNING AT LINE: %d in FILE: %s: %s", YELLOW, __LINE__, __FILE__, END);\
+            fprintf(stderr, __VA_ARGS__)
 
 #endif
